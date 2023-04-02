ef5309748fcbf646b09cce0e2331dd13dfef9742
Bacon <bacon.antife@gmail.com>
2023-04-02 00:33:12 -0400

    WÌP: vec3, matrix-math

--- scripts/vec3.js
+++ scripts/vec3.js
@@ -1,4 +1,4 @@
-import { rotXVec3 } from './matrix-math.js';
+import { rotXVec3, rotYVec3, rotZVec3 } from './matrix-math.js';
 
 class Vec3 extends Array {
 	constructor([ x = 0, y = 0, z = 0 ] = []) {
@@ -6,6 +6,14 @@ class Vec3 extends Array {
 	}
 	rotateX(angle, dst = this) {
 		rotXVec3(this, angle, dst);
+		return dst;
+	}
+	rotateY(angle, dst = this) {
+		rotYVec3(this, angle, dst);
+		return dst;
+	}
+	rotateZ(angle, dst = this) {
+		rotZVec3(this, angle, dst);
 		return dst;
 	}
 	get x() { return this[0]; }
