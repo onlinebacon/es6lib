7d5ded0b25271e0537aa94a16e4f07676cecc17d
Bacon <bacon.antife@gmail.com>
2023-04-10 09:54:31 -0400

    WIP: trig

--- scripts/trig.js
+++ scripts/trig.js
@@ -1,22 +1,24 @@
 const { PI, sin, cos, tan, asin, acos, atan } = Math;
 
 class Trig {
-	constructor(DEG = PI/180, RAD = 1, mul = 1, inv = 1) {
+	constructor(DEG = PI/180, RAD = 1, TO_RAD = 1, TO_DEG = 180/PI) {
 		this.DEG = DEG;
 		this.RAD = RAD;
-		this.mul = mul;
-		this.inv = inv;
+		this.TO_RAD = TO_RAD;
+		this.TO_DEG = TO_DEG;
 	}
-	sin(val) { return sin(val*this.mul); }
-	cos(val) { return cos(val*this.mul); }
-	tan(val) { return tan(val*this.mul); }
-	asin(val) { return asin(val)*this.inv; }
-	acos(val) { return acos(val)*this.inv; }
-	atan(val) { return atan(val)*this.inv; }
-	deg(val) { return val*this.DEG; }
-	rad(val) { return val*this.RAD; }
-	radians() { return new Trig(PI/180, 1, 1, 1); }
-	degrees() { return new Trig(1, 180/PI, PI/180, 180/PI); }
+	sin(val) { return sin(val*this.TO_RAD); }
+	cos(val) { return cos(val*this.TO_RAD); }
+	tan(val) { return tan(val*this.TO_RAD); }
+	asin(val) { return asin(val)*this.RAD; }
+	acos(val) { return acos(val)*this.RAD; }
+	atan(val) { return atan(val)*this.RAD; }
+	fromDeg(val) { return val*this.DEG; }
+	fromRad(val) { return val*this.RAD; }
+	inDeg(val) { return val*this.TO_DEG; }
+	inRad(val) { return val*this.TO_RAD; }
+	radians() { return new Trig(PI/180, 1, 1, 180/PI); }
+	degrees() { return new Trig(1, 180/PI, PI/180, 1); }
 }
 
 export default Trig;
